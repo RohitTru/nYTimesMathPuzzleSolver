ebca86f7a80ca61d88101d419640b60aeacd7d35
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-27 01:54:31 -0500

    get numbers function

--- main.py
+++ main.py
@@ -2,9 +2,38 @@
 def Introduction():
     print("Hi and welcome to the NYTIMES' Math Puzzle Solver")
 
+numbers = []
+
+def getNumbers():
+    i = 0
+    
+    print("Please enter your 6 integers: ")
+
+    while i < 6:
+        i += 1
+        numbers.append(int(input()))
+    
+    return(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
     Introduction()
+    getNumbers()
