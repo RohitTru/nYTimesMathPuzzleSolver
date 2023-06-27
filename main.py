6c277f1a5e78246017608ef40f71bc128ddf1f0f
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-27 02:34:24 -0500

    fixed global variable target and checkvalues()

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 
+numbers = []
+target = 0
+
 def introduction():
     print("Hi and welcome to the NYTIMES' Math Puzzle Solver")
 
-
-numbers = []
-
 def getNumbers():
     i = 0
-    
+    global target
+
     print("Please enter your 6 integers: ")
     while i < 6:
         i += 1
         numbers.append(int(input()))
     
     print("Please enter the Target value: ")
-    global target 
-    target = input()
+     
+    target = int(input())
     
     return(numbers, target)
 
-        
+def checkValues():
+    print(f'Are your values:  {numbers} and target value: {target} correct? (Enter "y" or "n")')
+    userResponse = input()
+
+
+
+
 
 
 
@@ -30,11 +37,9 @@ def getNumbers():
 
 
 def main():
-    introduction()
+    #introduction()
     getNumbers()
-
-    print(numbers)
-    print(target)
+    checkValues()
 
 
 
